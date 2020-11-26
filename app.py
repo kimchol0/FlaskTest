3ee7e61857f306cdf954c488837cd7e25660e13c
KimChol <kimchol2ece@outlook.com>
2020-11-26 13:21:03 +0800

    # 通过访问路径，获取用户的字符串、整形、浮点型参数

--- app.py
+++ app.py
@@ -3,9 +3,34 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# 路由解析，通过用户访问的路径，匹配相应的函数
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '你好！'
+
+
+@app.route("/index")
+def index():
+    return 'Hello'
+
+
+# 通过访问路径，获取用户的字符串参数
+@app.route("/user/<name>/<sex>")
+def welcome(name, sex):
+    return '你好，%s%s' % (name, sex)
+
+# 通过访问路径，获取用户的整形参数
+@app.route("/user/<int:id>")
+def welcome2(id):
+    return '你好，%d的会员' % id
+
+# 通过访问路径，获取用户的浮点型参数
+@app.route("/user/<float:id>")
+def welcome3(id):
+    return '您好，您的金额为：%f' % id
+
+# 注意，路由路径不能重复，用户通过唯一路径访问特定的函数
+
 
 
 if __name__ == '__main__':
