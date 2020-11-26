85876f20dbd791edde7568ab3ecaba27386f9d20
KimChol <kimchol2ece@outlook.com>
2020-11-26 13:56:51 +0800

    表单提交及接收提交的数据并显示出来

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import datetime
 
 app = Flask(__name__)
@@ -48,6 +48,20 @@ def index3():
     name = ['小张', '小王', '小赵']  # 列表类型
     task = {"任务": "打扫卫生", "时间": "3小时"}  # 字典类型
     return render_template("index.html", var=time, list=name, task=task)
+
+
+# 表单提交
+@app.route('/test/register')
+def register():
+    return render_template("test/register.html")
+
+
+# 注意，接收表单提交的路由，需要指定methods为post
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+        return render_template("test/result.html",result=result)
 
 
 if __name__ == '__main__':
