2d888de45c139b29b79467a98e879ea47fce3ddd
KimChol <kimchol2ece@outlook.com>
2020-11-26 13:41:09 +0800

    向页面传递变量

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
@@ -19,22 +20,34 @@ def index():
 def welcome(name, sex):
     return '你好，%s%s' % (name, sex)
 
+
 # 通过访问路径，获取用户的整形参数
 @app.route("/user/<int:id>")
 def welcome2(id):
     return '你好，%d的会员' % id
 
+
 # 通过访问路径，获取用户的浮点型参数
 @app.route("/user/<float:id>")
 def welcome3(id):
     return '您好，您的金额为：%f' % id
+
+
 # 注意，路由路径不能重复，用户通过唯一路径访问特定的函数
 
 # 返回给用户渲染后的网页文件
-@app.route("/")
+@app.route("/index2")
 def index2():
     return render_template("index.html")
 
+
+# 向页面传递一个变量
+@app.route("/index3")
+def index3():
+    time = datetime.date.today()  # 普通变量
+    name = ['小张', '小王', '小赵']  # 列表类型
+    task = {"任务": "打扫卫生", "时间": "3小时"}  # 字典类型
+    return render_template("index.html", var=time, list=name, task=task)
 
 
 if __name__ == '__main__':
